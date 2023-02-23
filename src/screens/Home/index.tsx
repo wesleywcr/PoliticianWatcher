bad8daf023ad92ba155eae5dad0d64836f42afff
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-02-22 23:29:08 -0300

    :recycle: refactor: screen home and new datails

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,69 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { ReactNode } from 'react';
-import { Dimensions, Text, View } from 'react-native';
+import { ActivityIndicator, Dimensions, Text, View } from 'react-native';
 
 import { Box, Container, Name } from './styles';
-import { FlashList } from "@shopify/flash-list";
+import { FlashList } from '@shopify/flash-list';
 import { useQuery, useQueryClient } from 'react-query';
 import axios from 'axios';
 import { CardDeputs } from '../../components/CardDeputs';
+import { SearchBar } from '../../components/SearchBar';
 
 const DATA = [
   {
-    title: "First Item",
+    title: 'First Item',
   },
   {
-    title: "Second Item",
+    title: 'Second Item',
   },
 ];
 
 export interface List {
-  id:number;
-  nome:string;
-  siglaPartido:string;
-  urlFoto:string;
-  
-  
+  id: number;
+  nome: string;
+  siglaPartido: string;
+  urlFoto: string;
 }
 const API_URL = 'https://dadosabertos.camara.leg.br/api/v2';
 export function Home() {
   const queryClient = useQueryClient();
+  const [searchDeputy, setSearchDeputy] = useState('');
 
-  const { data, isFetched, isLoading, error } = useQuery<List[]>(
-    ['deputy'],
+  const { data, isFetched, isLoading, isError, error } = useQuery<List[]>(
+    ['deputy', searchDeputy],
     async () => {
-      const response = await axios.get(`${API_URL}/deputados`);
+      const response = await axios.get(
+        `${API_URL}/deputados?idLegislatura=57&nome=${searchDeputy}`
+      );
       return response.data.dados;
-      //throw new Error('');
     },
     {
       //refetchOnWindowFocus: true,
-    // staleTime: 1000 * 30, //30seg
+      // staleTime: 1000 * 30, //30seg
       //refetchInterval: 1000 * 20, //20seg
       //retry: 4,
       //initialData:[]//sempre os isLoading vai ser false;
     }
   );
- //console.log('data',data[])
+  // console.log('data', data);
+
+  function handleSearch() {
+    console.log('Searching for', searchDeputy);
+    // setSearchText(search);
+  }
   return (
     <Container>
-      <Box>
-     <FlashList
-      data={data}
-      renderItem={({ item }) => 
-    
-      <CardDeputs 
-      key={item.id}
-      name={item.nome} 
-      party={item.siglaPartido} 
-      photo={item.urlFoto} 
-      
+      <SearchBar
+        placeholder="Pesquise por deputado"
+        value={searchDeputy}
+        onChangeText={setSearchDeputy}
+        onSubmitEditing={handleSearch}
       />
-    
-    }
-      estimatedItemSize={200}
-    />
-    </Box>
+      <Box>
+        {isLoading ? (
+          <ActivityIndicator color={'red'} />
+        ) : (
+          <FlashList
+            data={data}
+            renderItem={({ item }) => (
+              <CardDeputs
+                key={item.id}
+                name={item.nome}
+                party={item.siglaPartido}
+                photo={item.urlFoto}
+              />
+            )}
+            estimatedItemSize={200}
+          />
+        )}
+      </Box>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
 flex: 1;
-background-color: #000;
+background-color: #121212;
 justify-content: center;
 align-items: center;
 
@@ -13,6 +13,7 @@ export const Box = styled.View`
  width: ${Dimensions.get("screen").width};
   height: 70%;
  padding: 10px 10px;
+ margin-top: 70px;
  
   
 `;
