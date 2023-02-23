979e60409293b510fd857614f48419bd2f981554
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-02-22 23:26:20 -0300

    :sparkles: feat: add new fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { Text } from 'react-native';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Details } from './src/screens/Details';
 import { Home } from './src/screens/Home';
+import { useFonts } from 'expo-font';
 const queryClient = new QueryClient();
 
-export  function App() {
+export function App() {
+  const [fontsLoaded] = useFonts({
+    'Oswald-Regular': require('./src/assets/fonts/Oswald-Regular.ttf'),
+    'Oswald-Bold': require('./src/assets/fonts/Oswald-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
- <QueryClientProvider client={queryClient }>
-<Details/>
- </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Details />
+    </QueryClientProvider>
   );
 }
