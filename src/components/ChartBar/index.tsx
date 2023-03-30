accf209f86c5b2f2728873b1ec43b0b6542f6041
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-03-29 23:12:27 -0300

    :recycle: refactor: chart

--- src/components/ChartBar/index.tsx
+++ src/components/ChartBar/index.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { View } from "react-native";
 import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
+import { Container } from "./styles";
 
-const data = [
-  { quarter: 'Jan', earnings: 13000 },
-  { quarter: 'Fev', earnings: 16500 },
-  { quarter: 'Mar', earnings: 14250 },
-  { quarter: 'Abr', earnings: 19000 },
-  { quarter: 'Mai', earnings: 19000 },
-  { quarter: 'Jun', earnings: 19000 },
-  { quarter: 'Jul', earnings: 19000 },
-  { quarter: 'Ago', earnings: 19000 },
-  { quarter: 'Set', earnings: 19000 },
-  { quarter: 'Out', earnings: 19000 },
-  { quarter: 'Nov', earnings: 19000 },
-  { quarter: 'Dez', earnings: 19000 },
-];
 
-export function ChartBar() {
+interface Props{
+  data: {
+    quarter: string;
+    earnings: number;
+}[]
+}
+export function ChartBar({data}:Props) {
   return (
-    <View>
-      <VictoryChart theme={VictoryTheme.material}>
-        <VictoryBar data={data} x="quarter" y="earnings" />
+    <Container>
+      <VictoryChart theme={VictoryTheme.grayscale}  >
+        <VictoryBar data={data}
+          style={{ data: { fill: "#c43a31", } }}
+          animate={{
+            duration: 2000,
+            onLoad: { duration: 1000 }
+          }}
+        alignment="start"
+        x="quarter"   
+        y="earnings" />
       </VictoryChart>
-    </View>
+    </Container>
   );
 }
