1558450b4bab1572fd4f2fb3757b4b4b9b5cc6f5
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-03-29 23:08:13 -0300

    :sparkles: feat: add styles

---
+++ src/styles/Details/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+flex: 1;
+background-color: #121212;//#413F42
+padding: 16px;
+align-items: center;
+justify-content: center;
+`;
+export const Box = styled.View`
+align-items: center;
+margin-top: 50px;
+`
+export const BoxCard = styled.View`
+padding:16px;
+`
+
+export const Title = styled.Text`
+  color: #fff;
+  font-family: 'Oswald-Regular';
+  font-size: 34px;
+  text-align: center;
+`;
+export const Tot = styled.Text`
+  color: #fff;
+  font-family: 'Oswald-Bold';
+  font-size: 24px;
+  text-align: center;
+`;
